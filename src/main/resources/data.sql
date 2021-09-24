2776b651260799a6dbd0de9af497cac1031c72d6
Artur Sarakhman <artur.sarahman@gmail.com>
2021-09-24 16:35:36 +0300

    implement localization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,11 @@
 REPLACE INTO roles VALUES (1,'ADMIN');
 REPLACE INTO roles VALUES (2,'USER');
-REPLACE INTO roles VALUES (3,'BLOCKED');
 REPLACE INTO users VALUES (1, true, 'artur.sarahman@gmail.com', '$2a$12$IcsJIPLyMdHog5ntehnhW.9eMn7J9U1iR3d8hpoJHjWeCLavwdrSm', 'Artur Sarakhman');
 REPLACE INTO user_role VALUES (1, 1);
 REPLACE INTO users VALUES (2, true, 'user@gmail.com', '$2a$12$IcsJIPLyMdHog5ntehnhW.9eMn7J9U1iR3d8hpoJHjWeCLavwdrSm', 'Example User');
 REPLACE INTO user_role VALUES (2, 2);
-REPLACE INTO users VALUES (3, true, 'blocked@gmail.com', '$2a$12$IcsJIPLyMdHog5ntehnhW.9eMn7J9U1iR3d8hpoJHjWeCLavwdrSm', 'Blocked User');
-REPLACE INTO user_role VALUES (3, 3);
+REPLACE INTO users VALUES (3, false , 'blocked@gmail.com', '$2a$12$IcsJIPLyMdHog5ntehnhW.9eMn7J9U1iR3d8hpoJHjWeCLavwdrSm', 'Blocked User');
+REPLACE INTO user_role VALUES (2, 2);
 
 REPLACE INTO products VALUES (1, NOW(), 1000, 'Telephone');
 REPLACE INTO products VALUES (2, NOW(), 1500, 'TV');
